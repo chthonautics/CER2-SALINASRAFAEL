430881b4b3302721d41ccfd7909208b560d11e72
kanden <kanden@fecalfunny.com>
2024-10-17 12:04:22 -0300

    erectin a database

--- tiendaverde/sitio/models.py
+++ tiendaverde/sitio/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class users(models.Model):
+    firstname = models.CharField(max_length=64)
+    lastname = models.CharField(max_length=64)
+    email = models.CharField(max_length=256)
+    password_sha256 = models.CharField(max_length=64)
+
+class products(models.Model):
+    name = models.CharField(max_length=256)
+    price = models.IntegerField()
+
+class orders(models.Model):
+    user_id = models.IntegerField()
+    products = models.CharField(max_length=1024)
+    status = models.BooleanField()
